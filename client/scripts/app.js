8aad2f199fe1c8a77b42eb9301c8f3f5ccf4957e
samdsherman <samdsherman@gmail.com>
2016-09-27 12:33:19 -0700

    Add some styling and tabs that don't work yet

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,26 +6,36 @@ var app = {};
 //app properties/////////////////////////////////////////////////////
 
 app.server = 'https://api.parse.com/1/classes/messages';
-app.room = 'lobby';
+app.room = 'default';
 app.roomNames = {};
 app.friends = {};
 
 //app functions//////////////////////////////////////////////////////
-app.fetchSuccess = function fetchSuccess(data) {
+app.fetchSuccess = function fetchSuccess(data, urlParameter) {
   var messages = data.responseJSON.results;
+  // if (urlParameter) {
+  //   for (var i = messages.length - 1; i >= 0; i--) {
+  //     app.renderMessage(messages[i]);
+  //     app.renderRoom(messages[i].roomname);
+  //   }
+  // } else {
   messages.forEach(function(message) {
     app.renderMessage(message);
     //look at message.roomname
     app.renderRoom(message.roomname);
-  });
+  }); 
+  // }
   app.highlightFriends();
 };
 
 app.init = function init() {
-  $('body').on('click', '.username', app.handleUsernameClick);
+  $('body').on('click', '.panel-heading', app.handleUsernameClick);
   $('body').on('submit', '#send', app.handleSubmit );
   $('body').on('change', '#roomSelect', app.roomChange);
   $('body').on('click', '#refreshButton', app.refresh);
+  $('body').on('click', '.nav-tabs', function(event) {
+    $(this).tab('show');
+  });
 };
 
 app.send = function send(data) {
@@ -39,12 +49,14 @@ app.send = function send(data) {
   });
 };
 
-app.fetch = function fetch(handler) {
+app.fetch = function fetch(urlParameter) {
   // $.get(app.server, success);
+
+
   var data = $.ajax({
     type: 'GET',
-    url: app.server,
-    complete: handler || app.fetchSuccess
+    url: app.server + (urlParameter || ''),
+    complete: function(data) { app.fetchSuccess(data, urlParameter); }
   });
   return data;
 };
@@ -54,7 +66,7 @@ app.clearMessages = function clearMessages() {
 };
 
 app.handleUsernameClick = function handleUsernameClick() {
-  var $this = $(this);
+  var $this = $(this).find('.username');
   var username = $this.text();
   app.friends[username] = !app.friends[username];
   app.highlightFriends();
@@ -63,12 +75,15 @@ app.handleUsernameClick = function handleUsernameClick() {
 app.renderMessage = function renderMessage(message) {
 
   var $newMessage = $(
-    `<div class="chat panel panel-primary">
+    `<div class="chat panel panel-info">
       <div class="panel-heading">
+        <span class="at">@</span>
         <h3 class="username panel-title"></h3>
       </div>
-      <div class="text  panel-body"></div>
+      <div class="text panel-body"></div>
+      <div class="panel-footer"></div>
      </div>`);
+  $newMessage.find('.panel-footer').text(app.formatTime(message.createdAt));
   $newMessage.find('.username').text(message.username);
   $newMessage.find('.text').text(message.text);
 
@@ -76,6 +91,7 @@ app.renderMessage = function renderMessage(message) {
 };
 
 app.renderRoom = function renderRoom(roomName, setDefault) {
+  console.log(roomName);
   if ( app.roomNames[roomName] ) {
     return;
   }
@@ -95,20 +111,27 @@ app.roomChange = function roomChange() {
     app.room = window.prompt('New room name', 'Enter new room name');
     app.renderRoom(app.room, true);
   }
+  app.addTab();
 
   app.clearMessages();
-  if (app.room === 'lobby') {
-    app.fetch();
-  } else {
-    app.fetch(app.roomChangeFetchHandler);
-  }
+  var urlParameter = app.getRoomParameter();
+  console.log(urlParameter);
+
+  app.fetch( urlParameter );
   //selects the room you want as default
   $('#roomSelect').filter(function() {
     return $(this).val() === app.room;
   }).attr('selected', 'selected');
   app.highlightFriends();
-
 };
+
+app.addTab = function addTab() {
+  var newTab = $('<li role="presentation"><a role="tab" data-toggle="tab"></a></li>')
+                .attr('href', '#' + app.room);
+  newTab.find('a').text(app.room);
+  $('.nav-tabs').append(newTab);
+};
+
 
 app.roomChangeFetchHandler = function(data) {
   var messages = data.responseJSON.results;
@@ -152,11 +175,14 @@ app.getQueryVariable = function getQueryVariable(variable) {
 
 app.refresh = function refresh() {
   app.clearMessages();
-  if (app.room === 'lobby') {
-    app.fetch();
-  } else {
-    app.fetch(app.roomChangeFetchHandler);
-  }
+  var param = app.getRoomParameter();
+  console.log(param);
+  app.fetch(param);
+};
+
+app.getRoomParameter = function getRoomParameter() {
+  return app.room !== 'default' ? '?order=-createdAt&where={"roomname":"' + app.room + '"}'
+                                : '?order=-createdAt';
 };
 
 app.highlightFriends = function highlightFriends() {
@@ -174,7 +200,47 @@ app.highlightFriends = function highlightFriends() {
 };
 
 
+app.formatTime = function formatTime(timeString) {
+  var result = '';
+  var [date, time] = timeString.split('T');
+  var [year, month, day] = date.split('-');
+  var [hour, minute, second] = time.split(':');
 
+  result += app.monthToString(month);
+  result += ' ';
+  result += day;
+  result += ', ';
+  result += app.militaryHourTo12(hour, minute);
+
+  return result;
+};
+
+app.monthToString = function monthToString(monthVal) {
+  var months = {
+    '01': 'Jan',
+    '02': 'Feb',
+    '03': 'Mar',
+    '04': 'Apr',
+    '05': 'May',
+    '06': 'Jun',
+    '07': 'Jul',
+    '08': 'Aug',
+    '09': 'Sep',
+    '10': 'Oct',
+    '11': 'Nov',
+    '12': 'Dec'
+  };
+  return months[monthVal];
+};
+
+app.militaryHourTo12 = function militaryHourTo12(hour, minutes) {
+  var suffix = 'am';
+  if (+hour >= 12) {
+    suffix = 'pm';
+    hour = +hour - 12;
+  }
+  return hour + ':' + minutes + suffix;
+};
 
 
 
